018d179ab92af65d9cb29a772f19185ce5256446
gp_aranha <gianp_aranha@hotmail.com>
2024-07-22 20:06:22 -0300

    add create followers table sql command

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -11,3 +11,11 @@ CREATE TABLE users(
     password varchar(100) not null,
     created_at timestamp default current_timestamp()
 ) ENGINE=INNODB;
+
+CREATE TABLE followers(
+    user_id int not null,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    follower_id int not null,
+    FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+    primary key(user_id, follower_id)
+) ENGINE=INNODB;
